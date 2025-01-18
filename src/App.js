94bc534067fc72d46a5c8c6b796a2939b9546cd1
aldls <jealswl04@gmail.com>
2025-01-18 22:31:13 +0900

    kakaomap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+
 import { Route, Routes } from 'react-router-dom';
 import Search from './SearchTap';
+import Map from './MapTap';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/search" element={<Search />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </div>
   );
